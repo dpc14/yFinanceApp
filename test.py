d87483620122d03f2e202cbbad0efb551f2a99b6
dpc14 <14dpc14@gmail.com>
2020-06-30 10:36:01 -0400

    Initial Django app setup

---
+++ test.py
@@ -0,0 +1,21 @@
+import platform, datetime, yfinance as yf
+
+print('Python version = ' + platform.python_version())
+print('yfinance version = ' + yf.__version__)
+
+def yfinancetut(tickersymbol):
+    tickerdata = yf.Ticker(tickersymbol)
+    tickerinfo = tickerdata.info
+    investment = tickerinfo['shortName']
+    print('investment: ' + investment)
+
+    today = datetime.datetime.today().isoformat()
+
+    tickerDF = tickerdata.history(period='1d', start='2020-1-1',end=today[:10])
+    priceLast = tickerDF['Close'].iloc[-1]
+    priceYesterday = tickerDF['Close'].iloc[-2]
+    print(tickerDF)
+
+yfinancetut('TSLA') #Tesla
+print('')
+yfinancetut('VOO') #Vanguard S&P 500 Index ETF
