006bfdb16ff7860fdf5a4b94e06f34904bc09f00
dpc14 <14dpc14@gmail.com>
2020-07-02 10:39:42 -0400

    Add new app for stock data

--- yFinanceApp/yFinanceApp/settings.py
+++ yFinanceApp/yFinanceApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webApp.apps.webAppConfig',
+    'stocks.apps.StockConfig',
 ]
 
 MIDDLEWARE = [
--- yFinanceApp/yFinanceApp/urls.py
+++ yFinanceApp/yFinanceApp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webApp/', include('webApp.urls'))
+    path('webApp/', include('webApp.urls')),
+    path('stocks/', include('stocks.urls')),
 ]
