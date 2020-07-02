6b6e20129ae70a50974dd29ff51ecad9afc3c4d4
dpc14 <14dpc14@gmail.com>
2020-07-02 11:39:22 -0400

    Building out stocks app

--- yFinanceApp/stocks/urls.py
+++ yFinanceApp/stocks/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 app_name = 'stocks'
 urlpatterns = [
-    # path('', views.IndexView.as_view(), name='index'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # instead of <int:pk> should be stock ticker
 ]
--- yFinanceApp/stocks/views.py
+++ yFinanceApp/stocks/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'stocks/index.html'
+    context_object_name = 'latest_question_list'
+
+
+class DetailView(generic.DetailView):
+    model = Stock
+    template_name = 'stocks/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
